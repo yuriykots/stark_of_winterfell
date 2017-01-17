e89142599161a09639dfd7e78590977f0e52cb5d
yuriykots <setingm@gmail.com>
2017-01-17 15:45:15 -0500

    Updated title

--- src/js/Components/Header/Title.js
+++ src/js/Components/Header/Title.js
@@ -4,7 +4,7 @@ import React from "react";
 export default class Title extends React.Component{
   render(){
     return{
-      <h1> Welcome </h1>
+      <h1> Welcome to Starks of Winterfell </h1>
     }
   }
 }
