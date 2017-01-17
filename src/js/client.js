6bdb7d62e4e33ede26340c4491351667182c12b4
yuriykots <setingm@gmail.com>
2017-01-16 23:37:15 -0500

    adding footer, header

--- src/js/client.js
+++ src/js/client.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-
+import Layout from "./Components/Layout";
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
