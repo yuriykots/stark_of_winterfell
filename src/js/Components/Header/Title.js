99d6e85d700439bbbc37f420bfc28774b6734de6
yuriykots <setingm@gmail.com>
2017-01-20 20:28:50 -0500

    Added routes, getting character object

--- src/js/Components/Header/Title.js
+++ src/js/Components/Header/Title.js
@@ -4,7 +4,7 @@ import React from "react";
 export default class Title extends React.Component{
   render(){
     return (
-      <h1> Welcome to Starks of Winterfell </h1>
+      <h1> title component: Welcome to Starks of Winterfell </h1>
     )
   }
 }
--- src/js/Components/Layout.js
+++ src/js/Components/Layout.js
@@ -1,17 +1,23 @@
 import React from "react";
-import Footer from "./Footer";
+//this is the same folder
 import Header from "./Header";
+//this is one folder up / inside folder test
+//import Test from "../test/Test";
+//this is two folders up
+//import Test2 from "../../Test2";
+//this is two folders up / inside folder data
+//import Test3 from "../../data/Test3";
+//import characters from "../../data/characters";
+
 
 export default class Layout extends React.Component{
   render() {
+    console.log("hello")
+
     return (
     <div>
-    <h1> Layout is rendering. Yahoo </h1>
-
+    <h1> Layout is rendering </h1>
     </div>
     );
   }
 }
-
-//      <Header/>
-//      <Footer/>
