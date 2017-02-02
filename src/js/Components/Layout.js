49630b32984e3c737ed3ae1a3f7f87336726f0c6
yuriykots <setingm@gmail.com>
2017-02-01 23:00:58 -0500

    Added materialUI wrap on layout component

--- src/js/Components/Layout.js
+++ src/js/Components/Layout.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class Layout extends React.Component{
   render() {
     console.log("hello")
 
     return (
+<MuiThemeProvider>
     <div>
       <Header/>
       <div > {this.props.children} </div>
       <Footer/>
     </div>
+</MuiThemeProvider>
     );
   }
 }
