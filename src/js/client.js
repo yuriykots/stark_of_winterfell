4e99faf8f271405fa4b23ba86fde9814182c6917
yuriykots <setingm@gmail.com>
2017-02-10 15:15:25 -0500

    Splitting routes into module

--- src/js/client.js
+++ src/js/client.js
@@ -1,9 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Layout from "./Components/Layout";
-import IndexPage from "./Components/IndexPage";
-import NotFound from "./Components/NotFound";
-import CharacterPage from "./Components/CharacterPage";
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import routes from './routes';
 
@@ -14,8 +10,3 @@ ReactDOM.render(
   <Router routes={routes} history={browserHistory}>
   </Router>,
   app);
-
-//    <IndexRoute component={IndexPage}/>
-//    <Route path="character/:id" component = {CharacterPage}/>
-//    <Route path="*" component={NotFoundPage}/>
-//    <Route path="/character" component={CharacterPage}/>
