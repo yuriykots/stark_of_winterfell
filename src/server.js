f56934c6be9b75ccfbf892127ff1dc36aabbb721
yuriykots <setingm@gmail.com>
2017-02-12 15:35:29 -0500

    Creating markup and passing it to index.ejs

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var path = require('path')
 var compression = require('compression')
+import routes from './js/routes'
 
 var app = express()
 
@@ -12,9 +13,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'static')))
 
 // send all requests to index.html so browserHistory works
-app.get('/view', function (req, res) {
-  res.render('index');
+//app.get('/view', function (req, res) {
+  //res.render('index');
   //res.sendFile(path.join(__dirname, 'static', 'index.html'))
+//})
+
+app.get('*', (req, res) => {
+  // match the routes to the url
+  match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    // `RouterContext` is what the `Router` renders. `Router` keeps these
+    // `props` in its state as it listens to `browserHistory`. But on the
+    // server our app is stateless, so we need to use `match` to
+    // get these props before rendering.
+    const markup = renderToString(<RouterContext {...props}/>)
+
+    // dump the HTML into a template, lots of ways to do this, but none are
+    // really influenced by React Router, so we're just using a little
+    // function, `renderPage`
+    res.render('index', { markup });
+  })
 })
 
 var PORT = process.env.PORT || 8080
