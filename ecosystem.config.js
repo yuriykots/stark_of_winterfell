f4d2ecbdf5222f8b857660039ac14b7f7084c13c
yuriykots <setingm@gmail.com>
2017-02-14 23:42:35 -0500

    Changed path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -27,7 +27,7 @@ module.exports = {
       host : "45.55.154.93",
       ref  : "origin/master",
       repo : "https://github.com/yuriykots/stark_of_winterfell.git",
-      path : "~/stark_of_winterfell",
+      path : "~/stark",
       "post-deploy" : "nvm install && npm install && pm2 startOrRestart ecosystem.json --env production"
     }
   }
